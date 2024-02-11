e4fed49b10b309666ac0c6a5e86cedc15d46c7ae
komarnetwork <65493176+komarnetwork@users.noreply.github.com>
2024-02-11 10:20:14 +0700

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+const nav = document.getElementById("nav");
+const navMenu = document.getElementById("navMenu");
+const hamburgerMenu = document.getElementById("hamburgerMenu");
+
+// box shadow navigation aktiv pada saat window di scroll
+window.onscroll = function () {
+  let scrollOn = window.pageYOffset;
+  if (scrollOn > 20) {
+    nav.style.backgroundColor = "var(--white)";
+    nav.style.boxShadow = "0 4px 20px rgb(0, 0, 0, 0.2)";
+    nav.style.transition = "200ms";
+  } else {
+    nav.style.backgroundColor = "transparent";
+    nav.style.boxShadow = "none";
+  }
+};
+
+// navigation dan hamburger menu untuk mobile
+hamburgerMenu.addEventListener("click", function () {
+  hamburgerMenu.classList.toggle("hamburgerMenuActive");
+  navMenu.classList.toggle("navMenuActive");
+});
