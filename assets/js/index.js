0ef8ac53ca55c8d05dc4bf581cc8d113e645e974
komarnetwork <komar.network@gmail.com>
2024-08-13 12:52:24 +0700

    updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,44 +1,44 @@
-const nav = document.getElementById("nav");
-const navMenu = document.getElementById("navMenu");
-const hamburgerMenu = document.getElementById("hamburgerMenu");
-const tabs = document.querySelectorAll(".tabs li");
-const contents = document.querySelectorAll(".content");
-const modalViews = document.querySelectorAll(".modal");
-const triggerBtn = document.querySelectorAll(".trigger");
-const closeButton = document.querySelectorAll(".close-button");
+const nav = document.getElementById('nav');
+const navMenu = document.getElementById('navMenu');
+const hamburgerMenu = document.getElementById('hamburgerMenu');
+const tabs = document.querySelectorAll('.tabs li');
+const contents = document.querySelectorAll('.content');
+const modalViews = document.querySelectorAll('.modal');
+const triggerBtn = document.querySelectorAll('.trigger');
+const closeButton = document.querySelectorAll('.close-button');
 
 // box shadow navigation aktiv pada saat window di scroll
 window.onscroll = function () {
   let scrollOn = window.pageYOffset;
   if (scrollOn > 20) {
-    nav.style.backgroundColor = "var(--white)";
-    nav.style.boxShadow = "0 4px 20px rgb(0, 0, 0, 0.2)";
-    nav.style.transition = "200ms";
+    nav.style.backgroundColor = 'var(--white)';
+    nav.style.boxShadow = '0 4px 20px rgb(0, 0, 0, 0.2)';
+    nav.style.transition = '200ms';
   } else {
-    nav.style.backgroundColor = "transparent";
-    nav.style.boxShadow = "none";
+    nav.style.backgroundColor = 'transparent';
+    nav.style.boxShadow = 'none';
   }
 };
 
 // navigation dan hamburger menu untuk mobile
-hamburgerMenu.addEventListener("click", function () {
-  hamburgerMenu.classList.toggle("hamburgerMenuActive");
-  navMenu.classList.toggle("navMenuActive");
+hamburgerMenu.addEventListener('click', function () {
+  hamburgerMenu.classList.toggle('hamburgerMenuActive');
+  navMenu.classList.toggle('navMenuActive');
 });
 
 // tabs
 tabs.forEach((tab, index) => {
-  tab.addEventListener("click", () => {
+  tab.addEventListener('click', () => {
     // hapus class active dari sebelumnya di klik
-    tabs.forEach((tab) => tab.classList.remove("active"));
+    tabs.forEach((tab) => tab.classList.remove('active'));
 
-    tab.classList.add("active");
+    tab.classList.add('active');
 
     // hide dari content sebelumnya
-    contents.forEach((c) => c.classList.remove("active"));
+    contents.forEach((c) => c.classList.remove('active'));
 
     // menampilkan content dari tab yang terseleksi
-    contents[index].classList.add("active");
+    contents[index].classList.add('active');
   });
 });
 
@@ -46,20 +46,20 @@ tabs[0].click();
 
 // portofolio modal/popup
 var modal = function (modalClick) {
-  modalViews[modalClick].classList.add("active");
+  modalViews[modalClick].classList.add('active');
 };
 
 triggerBtn.forEach((triggerBtn, i) => {
-  triggerBtn.addEventListener("click", () => {
+  triggerBtn.addEventListener('click', () => {
     modal(i);
     // alert("hello");
   });
 });
 
 closeButton.forEach((closeBtn) => {
-  closeBtn.addEventListener("click", () => {
+  closeBtn.addEventListener('click', () => {
     modalViews.forEach((modalView) => {
-      modalView.classList.remove("active");
+      modalView.classList.remove('active');
     });
   });
 });
@@ -72,25 +72,25 @@ let isOpen = false;
 let overlay;
 
 function createOverlay() {
-  overlay = document.createElement("zoomable-image-overlay");
+  overlay = document.createElement('zoomable-image-overlay');
   document.body.appendChild(overlay);
 }
 
 class ZoomableImageOverlay extends HTMLElement {
   connectedCallback() {
-    const style = document.createElement("style");
+    const style = document.createElement('style');
     style.textContent = ZoomableImageOverlay.style;
 
-    const shadow = this.attachShadow({ mode: "open" });
+    const shadow = this.attachShadow({ mode: 'open' });
     shadow.appendChild(style);
   }
 
   show() {
-    this.setAttribute("shown", "");
+    this.setAttribute('shown', '');
   }
 
   hide() {
-    this.removeAttribute("shown");
+    this.removeAttribute('shown');
   }
 
   static get style() {
@@ -123,19 +123,19 @@ class ZoomableImageOverlay extends HTMLElement {
 class ZoomableImage extends HTMLElement {
   isOpen = false;
   connectedCallback() {
-    this.image = this.querySelector("img");
-    overlay.addEventListener("click", this.close.bind(this));
-    window.addEventListener("scroll", this.close.bind(this));
+    this.image = this.querySelector('img');
+    overlay.addEventListener('click', this.close.bind(this));
+    window.addEventListener('scroll', this.close.bind(this));
 
-    const style = document.createElement("style");
+    const style = document.createElement('style');
     style.textContent = ZoomableImage.style;
 
-    const shadow = this.attachShadow({ mode: "open" });
+    const shadow = this.attachShadow({ mode: 'open' });
     shadow.appendChild(style);
     shadow.appendChild(this.image);
 
-    this.addEventListener("click", this.toggle.bind(this));
-    window.addEventListener("resize", this.updateTransform.bind(this));
+    this.addEventListener('click', this.toggle.bind(this));
+    window.addEventListener('resize', this.updateTransform.bind(this));
   }
 
   toggle() {
@@ -146,8 +146,8 @@ class ZoomableImage extends HTMLElement {
     isOpen = true;
     this.isOpen = true;
     overlay.show();
-    this.setAttribute("open", "");
-    this.image.style.zIndex = "2";
+    this.setAttribute('open', '');
+    this.image.style.zIndex = '2';
     this.originalImageRect = this.image.getBoundingClientRect();
     this.updateTransform();
   }
@@ -175,13 +175,13 @@ class ZoomableImage extends HTMLElement {
     if (!this.isOpen) return;
     this.isOpen = false;
     isOpen = false;
-    this.removeAttribute("open");
+    this.removeAttribute('open');
     const onTransitionEnd = () => {
-      this.image.style.zIndex = "";
-      this.image.removeEventListener("transitionend", onTransitionEnd);
+      this.image.style.zIndex = '';
+      this.image.removeEventListener('transitionend', onTransitionEnd);
     };
-    this.image.addEventListener("transitionend", onTransitionEnd);
-    this.image.style.transform = "none";
+    this.image.addEventListener('transitionend', onTransitionEnd);
+    this.image.style.transform = 'none';
     overlay.hide();
   }
 
@@ -208,5 +208,5 @@ class ZoomableImage extends HTMLElement {
 
 createOverlay();
 
-customElements.define("zoomable-image", ZoomableImage);
-customElements.define("zoomable-image-overlay", ZoomableImageOverlay);
+customElements.define('zoomable-image', ZoomableImage);
+customElements.define('zoomable-image-overlay', ZoomableImageOverlay);
